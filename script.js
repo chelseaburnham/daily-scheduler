5683b95eb5be429902c81536b3fe25854fc6d3ea
Chelsea Burnham <chelseaburnham0@gmail.com>
2022-06-29 18:58:04 -0700

    fix buttons to take up entire space

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 //changes the day to the current day
 var today = moment()
-$("#currentDay").text(today.format("MMM Do, YYYY"))
+$("#currentDay").text(today.format("MMMM Do, YYYY"))
 
+//colors everything in the past red
+function pastTime() {
+
+}
+
+//colors everything in the past red
+function pastTime() {
+
+}
+
+//colors everything in the past red
+function pastTime() {
+
+}
